fd473bb5cd9e01efc849205ce8e1b20b0c1773f3
Maruti <sohamdhanduke.2304@gmail.com>
2026-02-06 23:12:50 +0530

    Q6: Shared Library

---
+++ Assignment_8/Question_6/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "math.h"
+
+int main()
+{
+    int a = 11;
+    int b = 21;
+
+    printf("Add = %d\n",Add(a,b));
+    printf("Sub = %d\n",Sub(a,b));
+    printf("Mul = %d\n",Mul(a,b));
+    printf("Div = %d\n",Div(a,b));
+
+    return 0;
+}
